622ca43118da874457df190cbb7cd6da6c57494a
Alexander Schieving <alexanderschieving@gmail.com>
2017-07-03 08:40:07 +0200

    Create and show new Carmodel pages

--- app/controllers/carmodels_controller.rb
+++ app/controllers/carmodels_controller.rb
@@ -4,4 +4,24 @@ class CarmodelsController < ApplicationController
       @carmodels = Carmodel.all
     end
 
+    def show
+        @carmodel = Carmodel.find(params[:id])
+      end
+
+    def new
+      @carmodel = Carmodel.new
+    end
+
+    def create
+    carmodel_params = params.require(:carmodel).permit(:name, :bodystyle, :horsepower, :image_url)
+
+    @carmodel = Carmodel.new(carmodel_params)
+
+    if @carmodel.save
+       redirect_to @carmodel
+    else
+       render 'new'
+    end
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,9 @@ Rails.application.routes.draw do
   root "carmodels#index"
 
    get "carmodels" => "carmodels#index"
+   get 'carmodels/new' => "carmodels#new", as: :new_carmodel
+   get "carmodels/:id" => "carmodels#show", as: :carmodel
+
+   post "carmodels" => "carmodels#create"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
